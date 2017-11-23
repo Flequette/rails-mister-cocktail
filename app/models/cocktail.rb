7b7ea5fcf3b77baa8abf02262d23e14ddac1d7ce
Florine Lequette <florine.lequette@gmail.com>
2017-11-23 12:23:06 +0100

    cocktail party

---
+++ app/models/cocktail.rb
@@ -0,0 +1,6 @@
+class Cocktail < ApplicationRecord
+  has_many :doses, dependent: :destroy
+  has_many :ingredients, dependent: :destroy, through: :doses
+
+  validates :name, uniqueness: true, presence: true
+end
